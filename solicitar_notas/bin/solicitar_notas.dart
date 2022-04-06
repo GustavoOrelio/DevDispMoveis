22d812133fc79cac2e980d9268d59a0f5d4ced3a
GustavoOrelio <oreliogustavo@gmail.com>
2022-04-05 22:40:19 -0300

    A media do exercicio já foi calculada, falta achar o maior e o menor valor

---
+++ solicitar_notas/bin/solicitar_notas.dart
@@ -0,0 +1,5 @@
+import 'package:solicitar_notas/solicitarNotas.dart' as solicitarNotas;
+
+void main(List<String> arguments) {
+  solicitarNotas.solicitarNotas();
+}
---
+++ solicitar_notas/lib/solicitarNotas.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void solicitarNotas() {
+  String? nota1;
+  String? nota2;
+  String? nota3;
+
+  print("Informe a primeira nota do aluno: ");
+  nota1 = stdin.readLineSync();
+  double notaA = double.parse(nota1!);
+
+  print("Informe a segunda nota do aluno: ");
+  nota2 = stdin.readLineSync();
+  double notaB = double.parse(nota2!);
+
+  print("Informe a terceira nota do aluno: ");
+  nota3 = stdin.readLineSync();
+  double notaC = double.parse(nota3!);
+
+  double media = notaA + notaB + notaC / 3;
+
+  print("\nA primeira nota do aluno é: $notaA");
+  print("\nA segunda nota do aluno é: $notaB");
+  print("\nA terceira nota do aluno é: $notaC");
+  print("\nA media das notas do aluno é: $media");
+}
