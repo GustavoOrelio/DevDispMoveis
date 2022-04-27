a0af0974f33614800b4b273b83df7d297ca86893
Gustavo Orelio <oreliogustavo@gmail.com>
2022-04-26 22:18:34 -0300

    Finalizando atividade 2

--- Aula.26.04/atividade_02/bin/atividade_02.dart
+++ Aula.26.04/atividade_02/bin/atividade_02.dart
@@ -1,5 +1,3 @@
 import 'package:atividade_02/atividade_02.dart' as atividade_02;
 
-void main(List<String> arguments) {
-  atividade_02.show();
-}
+void main(List<String> arguments) {}
--- Aula.26.04/atividade_02/lib/atividade_02.dart
+++ Aula.26.04/atividade_02/lib/atividade_02.dart
@@ -4,8 +4,6 @@ void main() {
 //Exercício 1 - faça a chamada do método adicionarSalario validando se o valor é positivo.
   var salario = 1500.00;
   var bonus = 150.00;
-
-  String resultado = "";
   adicionarSalario(salario, bonus, ePositivo(bonus));
 //Exercício 2 - refaça o exe anterior, convertendo a named function em anonymous function.
   adicionarSalario(salario, bonus, (bonus) {
@@ -20,9 +18,11 @@ void main() {
 //Exercício 4  - refaça os exe's a, b e c, porém validando se o valor é menor do que o salário.
   adicionarSalario(salario, bonus, (bonus) => (bonus < salario));
 //Exercício 5  - refaça o exe c porém fazendo uma validação sua.
-  
+  adicionarSalario(salario, bonus, (bonus) => (bonus >= salario));
 //Exercício 6 - faça 2 exemplos do uso de funções anônimas utilizando a biblioteca do dart.
-
+  var nota1 = 1500.00;
+  var nota2 = 150.00;
+  mediaNotas(nota1, nota2, (media) => (nota1 + nota2) / 2);
 /*
   Exercício 6 - Conseguiu entender:
     - O contexto em que é útil utilizar funções anônimas?
@@ -32,6 +32,8 @@ void main() {
     Em que caso devemos utilizar Arrow Function? O que devemos considerar?
   */
 }
+
+void mediaNotas(double nota1, double nota2, Function(double media)) {}
 
 void adicionarSalario(
     double salario, double valor, Function(double valor) validacao) {
