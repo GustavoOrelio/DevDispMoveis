4e99e6a140d8dbc96e60979c5a6a38c0985caefb
Gustavo Orelio <oreliogustavo@gmail.com>
2022-04-26 21:34:33 -0300

    Funções com parametros nomeados e misturados

---
+++ Aula.26.04/funcao/bin/funcao.dart
@@ -0,0 +1,5 @@
+import 'package:funcao/funcao.dart' as funcao;
+
+void main(List<String> arguments) {
+  funcao.main();
+}
---
+++ Aula.26.04/funcao/lib/funcao.dart
@@ -0,0 +1,36 @@
+void main() {
+  verificarAprovacao(5, 7, 7);
+  verificarAprovacao(6, 8);
+
+  verificarAprovacaoComParamNomeado(nota1: 8, nota2: 9, mediaAprovacao: 7);
+}
+
+String verificarAprovacao(double nota1, double nota2,
+    [double mediaAprovacao = 6]) {
+  var media = (nota1 + nota2) / 2;
+  if (media >= mediaAprovacao) {
+    return 'aprovado';
+  } else {
+    return 'reprovado';
+  }
+}
+
+String verificarAprovacaoComParamNomeado(
+    {required double nota1, required double nota2, double mediaAprovacao = 6}) {
+  var media = (nota1 + nota2) / 2;
+  if (media >= mediaAprovacao) {
+    return 'aprovado';
+  } else {
+    return 'reprovado';
+  }
+}
+
+String verificarAprovacaoComParamMisturado(double nota1,
+    {required double nota2, double mediaAprovacao = 6}) {
+  var media = (nota1 + nota2) / 2;
+  if (media >= mediaAprovacao) {
+    return 'aprovado';
+  } else {
+    return 'reprovado';
+  }
+}
